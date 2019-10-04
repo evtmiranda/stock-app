7ece7e9785b0bf41fdef9850bc22a6f17a0d7545
Everton Oliveira <notreve.miranda@gmail.com>
2019-10-04 11:12:35 -0300

    [FIX] Fix get users
    * Fix method loadUsers to list only active users;
    * Remove commented codes of user.service.js;

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,3 @@
-// import config from 'config';
 import { authHeader } from '../helpers';
 import { api } from '../services'
 import { isUndefined } from 'util';
@@ -25,7 +24,6 @@ async function authenticate(username, password) {
 }
 
 function logout() {
-    // remove user from local storage to log user out
     localStorage.removeItem('user');
 }
 
@@ -52,30 +50,3 @@ async function remove(id) {
 
     return rowsDeleted;
 }
-
-// function get() {
-//     const requestOptions = {
-//         method: 'GET',
-//         headers: authHeader()
-//     };
-
-//     return fetch(`${config.apiUrl}/users`, requestOptions).then(handleResponse);
-// }
-
-// function handleResponse(response) {
-//     return response.text().then(text => {
-//         const data = text && JSON.parse(text);
-//         if (!response.ok) {
-//             if (response.status === 401) {
-//                 // auto logout if 401 response returned from api
-//                 logout();
-//                 location.reload(true);
-//             }
-
-//             const error = (data && data.message) || response.statusText;
-//             return Promise.reject(error);
-//         }
-
-//         return data;
-//     });
-// }
