44e66f7c471088710d8161d7058745f7478ad186
Everton Oliveira <notreve.miranda@gmail.com>
2019-10-31 10:48:48 -0300

    [FEAT] Render front with permissions

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { getAuthenticatedUser } from '../../utils'
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={props => (
-        localStorage.getItem('user')
+        getAuthenticatedUser()
             ? <Component {...props} />
             : <Redirect to={{ pathname: '/', state: { from: props.location } }} />
     )} />
---
+++ src/utils/getAuthenticatedUser.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-undef */
+export default () => {
+    return JSON.parse(localStorage.getItem('user'))
+}
