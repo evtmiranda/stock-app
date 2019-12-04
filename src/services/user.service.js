f2296dce13a8be02c3fe48f35c1b62465ae1201b
Everton Oliveira <notreve.miranda@gmail.com>
2019-12-04 18:42:14 -0300

    [FEAT] Shows api errors
    * Now the errors api will be displayed to the user;

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -42,51 +42,82 @@ function userHasPermission(moduleId, permissionId) {
 }
 
 async function get(filters) {
-    const config = {
-        headers: authHeader()
-    };
+    try {
+        const config = {
+            headers: authHeader()
+        };
 
-    const response = await api.get(`v1/users?${filters}`, config)
+        const response = await api.get(`v1/users?${filters}`, config)
 
-    const users = response.data;
+        const users = response.data;
 
-    return users;
+        return users;
+    } catch (error) {
+        return errorResponse(error)
+    }
 }
 
 async function remove(id) {
-    const config = {
-        headers: authHeader()
-    };
+    try {
+        const config = {
+            headers: authHeader()
+        };
 
-    const response = await api.delete(`v1/users/${id}`, config)
+        const response = await api.delete(`v1/users/${id}`, config)
 
-    const rowsDeleted = response.data;
+        const rowsDeleted = response.data;
 
-    return rowsDeleted;
+        return rowsDeleted;
+    } catch (error) {
+        return errorResponse(error)
+    }
 }
 
 async function create(user) {
-    const config = {
-        headers: authHeader()
-    };
+    try {
+        const config = {
+            headers: authHeader()
+        };
 
-    const response = await api.post(`v1/users`, user, config)
+        const response = await api.post(`v1/users`, user, config)
 
-    const result = response.data;
+        const result = response.data;
 
-    return result;
+        return result;
+    } catch (error) {
+        return errorResponse(error)
+    }
 }
 
 async function update(user) {
-    const config = {
-        headers: authHeader()
-    };
+    try {
+        const config = {
+            headers: authHeader()
+        };
 
-    const response = await api.put(`v1/users/${user.id}`, user, config)
+        const response = await api.put(`v1/users/${user.id}`, user, config)
 
-    const rowsUpdated = response.data;
+        const rowsUpdated = response.data;
 
-    return rowsUpdated;
+        return rowsUpdated;
+    } catch (error) {
+        return errorResponse(error)
+    }
+}
+
+function errorResponse(error) {
+    if (error.response) {
+        return error.response.data
+    }
+    else {
+        return {
+            errors: [
+                {
+                    erro: "Oops, ocorreu algo inesperado, por favor, tente novamente"
+                }
+            ]
+        }
+    }
 }
 
 export const userService = {
